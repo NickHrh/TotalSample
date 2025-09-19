c5369cf16e82e61b35b3fb2e22dd2be677717703
NickHrh <kurode@163.com>
2025-09-19 10:54:20 +0800

    1.基本完成自定义圆形ImageView效果

--- app/src/main/java/com/example/totalsample/custom/view/CircleImageView.kt
+++ app/src/main/java/com/example/totalsample/custom/view/CircleImageView.kt
@@ -1,37 +1,54 @@
 package com.example.totalsample.custom.view
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffXfermode
 import android.graphics.RectF
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
 import com.example.totalsample.R
 
 class CircleImageView : AppCompatImageView {
 
+    private var borderWidth = 0f
+    private var borderColor = Color.WHITE
+
+    private var shadowRadius = 0f
+    private var shadowColor = Color.TRANSPARENT
+
+
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     val borderPaint = Paint(Paint.ANTI_ALIAS_FLAG)
     val shadowPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    // 用于离屏缓冲的paint
+    private val layerPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+
     val rect = RectF()
-    val srcRect = RectF()
-    val destRect = RectF()
 
-    constructor(ctx: Context) : super(ctx)
+    constructor(ctx: Context) : super(ctx) {
+        init(null)
+    }
 
-    constructor(ctx: Context, attrs: AttributeSet?) : super(ctx, attrs)
+    constructor(ctx: Context, attrs: AttributeSet?) : super(ctx, attrs) {
+        init(attrs)
+    }
 
     constructor(ctx: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
         ctx,
         attrs,
         defStyleAttr
-    )
-
-
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    ) {
+        init(attrs)
     }
+
+
+    val srcInMode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
 
     override fun onDraw(canvas: Canvas) {
         if (drawable == null) {
@@ -40,21 +57,62 @@ class CircleImageView : AppCompatImageView {
         }
 
         val diameter =
-            (width - paddingLeft - paddingRight).coerceAtMost(height - paddingTop - paddingBottom)
+            (width - paddingLeft - paddingRight)
+                .coerceAtMost(height - paddingTop - paddingBottom)
+        val radius = diameter / 2f
+        val centerX = width / 2f
+        val centerY = height / 2f
 
+        if (shadowRadius > 0) {
+            shadowPaint.color = shadowColor
+            canvas.drawCircle(centerX, centerY, radius, shadowPaint)
+        }
 
+        val saveCount = canvas.saveLayer(0f, 0f, width.toFloat(), height.toFloat(), layerPaint)
+
+        paint.reset()
+        paint.isAntiAlias = true
+        paint.color = Color.BLACK
+        canvas.drawCircle(centerX, centerY, radius, paint)
+
+        paint.xfermode = srcInMode
+
+        val bmp = drawableToBitmap(drawable, diameter, diameter)
+        canvas.drawBitmap(bmp, centerX - radius, centerY - radius, paint)
+
+        paint.xfermode = null
+        canvas.restoreToCount(saveCount)
+
+        if (borderWidth > 0) {
+            borderPaint.isAntiAlias = true
+            borderPaint.style = Paint.Style.STROKE
+            borderPaint.strokeWidth = borderWidth
+            borderPaint.color = borderColor
+            canvas.drawCircle(centerX, centerY, radius - borderWidth / 2, borderPaint)
+        }
     }
 
-    private fun init(attrs: AttributeSet) {
+    private fun drawableToBitmap(drawable: Drawable, w: Int, h: Int): Bitmap {
+        val bmp = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        val c = Canvas(bmp)
+        drawable.setBounds(0, 0, w, h)
+        drawable.draw(c)
+        return bmp
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        rect.set(0f, 0f, w.toFloat(), h.toFloat())
+    }
+
+    private fun init(attrs: AttributeSet?) {
 
         scaleType = ScaleType.CENTER_CROP
         val typeArray = context.obtainStyledAttributes(attrs, R.styleable.CircleImageView)
-        val borderWidth = typeArray.getDimension(R.styleable.CircleImageView_borderWidth, 0f)
-        val borderColor =
-            typeArray.getColor(R.styleable.CircleImageView_borderColor, Color.WHITE)
-        val shadowRadius = typeArray.getDimension(R.styleable.CircleImageView_shadowRadius, 0f)
-        val shadowColor =
-            typeArray.getColor(R.styleable.CircleImageView_shadowColor, Color.TRANSPARENT)
+        borderWidth = typeArray.getDimension(R.styleable.CircleImageView_borderWidth, 0f)
+        borderColor = typeArray.getColor(R.styleable.CircleImageView_borderColor, Color.WHITE)
+        shadowRadius = typeArray.getDimension(R.styleable.CircleImageView_shadowRadius, 0f)
+        shadowColor = typeArray.getColor(R.styleable.CircleImageView_shadowColor, Color.TRANSPARENT)
         typeArray.recycle()
 
         borderPaint.apply {
@@ -64,8 +122,6 @@ class CircleImageView : AppCompatImageView {
         }
 
         shadowPaint.color = shadowColor
-
+        paint.color = Color.WHITE
     }
-
-
 }
--- app/src/main/java/com/example/totalsample/custom/view/CustomViewActivity.kt
+++ app/src/main/java/com/example/totalsample/custom/view/CustomViewActivity.kt
@@ -1,20 +1,17 @@
 package com.example.totalsample.custom.view
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Toast
 import com.example.totalsample.BaseActivity
-import com.example.totalsample.R
 import com.example.totalsample.databinding.ActivityCustomViewBinding
-import com.example.totalsample.databinding.ActivityMessagerBinding
 
-class CustomViewActivity : BaseActivity<ActivityCustomViewBinding>()  {
+class CustomViewActivity : BaseActivity<ActivityCustomViewBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        vBinding
+        vBinding.circleIv.setOnClickListener {
+            Toast.makeText(this, "Show Circle ImageView!!!", Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
