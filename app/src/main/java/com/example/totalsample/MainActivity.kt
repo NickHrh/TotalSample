751c4fa5a9ae3cb8f118e83f1bda68eb13a2f145
NickHrh <kurode@163.com>
2025-09-24 13:10:50 +0800

    1.输入数字n，救1-n相加的和

--- app/src/main/java/com/example/totalsample/MainActivity.kt
+++ app/src/main/java/com/example/totalsample/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.totalsample
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import com.example.totalsample.custom.view.CustomViewActivity
 import com.example.totalsample.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
+
+    val TAG = MainActivity::class.simpleName
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +25,36 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
             btnThree.setOnClickListener {
                 startActivity(Intent(this@MainActivity, CustomViewActivity::class.java))
             }
+
+            btnFour.setOnClickListener {
+                sum(100).also {
+                    Log.i(TAG, "get the sum:$it")
+                }
+            }
+        }
+    }
+
+    //输入数字n，救1-n相加的和
+    fun sum(n: Int): Int {
+        val median = n / 2
+        val divisible = n % 2 == 0
+        var temp = 0;
+        for (i in 0 until n) {
+            temp += (n - i) + (n - (n - 1 - i))
+            Log.i(TAG, "The sum of the $i: $temp ")
+            if (i == median - 1) {
+                if (divisible) {
+                    break
+                } else {
+                    temp += n - median
+                    Log.i(TAG, "Add the median ${n - median},sum:$temp")
+                    break
+                }
+            }
         }
 
-
+        return temp
     }
+
+
 }
